5c50970df4f791b9a05392c0ef8e332b6268b1ab
prateekgupta <prateekgupta291@gmail.com>
2023-01-04 17:42:23 +0530

    added impl for get route

--- src/main/scala/com/knoldus/aws/examples/routes/QuestionAPIImpl.scala
+++ src/main/scala/com/knoldus/aws/examples/routes/QuestionAPIImpl.scala
@@ -11,19 +11,30 @@ class QuestionAPIImpl(questionService: QuestionService) extends QuestionAPI with
   val routes: Route = submitQuestion ~ getQuestion ~ updateQuestion() ~ deleteQuestion()
 
   override def submitQuestion: Route =
-    path("question") {
+    path("questions" / "submit") {
       pathEnd {
         (post & entity(as[Question])) { questionSubmissionRequest =>
-          logger.info(s"Making request for question submission")
+          logger.info("Making request for question submission")
           val response = questionService.submitQuestion(questionSubmissionRequest)
           complete(response)
         }
       }
     }
 
-  override def getQuestion: Route = ???
+  override def getQuestion: Route =
+    path("questions" / Segment / Segment) { (category, questionId) =>
+      pathEnd {
+        get {
+          logger.info(s"Making request for getting the question with category: $category and id: $questionId")
+          val response = questionService.getQuestion(questionId, category)
+          complete(response)
+        }
+      }
+    }
 
-  override def updateQuestion(): Route = ???
+  override def updateQuestion(): Route =
+    ???
 
-  override def deleteQuestion(): Route = ???
+  override def deleteQuestion(): Route =
+    ???
 }
