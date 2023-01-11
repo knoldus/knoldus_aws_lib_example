94268c20ea4315f1abe00f6c99bf979db530d57e
prateekgupta <prateekgupta291@gmail.com>
2023-01-11 16:49:06 +0530

    added new method

--- src/main/scala/com/knoldus/aws/services/kinesis/BankAccountEventGenerator.scala
+++ src/main/scala/com/knoldus/aws/services/kinesis/BankAccountEventGenerator.scala
@@ -12,24 +12,25 @@ class BankAccountEventGenerator(bankAccountEventPublisher: BankAccountEventPubli
   def createBankAccountEvent(bankAccountDetails: BankAccountCreationRequest): Future[BankAccountEvent] = {
     val newAccountNumber = UUID.randomUUID()
     val createBankAccountEvent = CreateBankAccountEvent(newAccountNumber, bankAccountDetails.accountOwner, bankAccountDetails.accountOwner, bankAccountDetails.securityCode, bankAccountDetails.initialBalance)
-    val isEventPublishedFuture = bankAccountEventPublisher.publishBankAccountEvent(stream, createBankAccountEvent)
-
+    val publishEventResultFuture = bankAccountEventPublisher.publishBankAccountEvent(stream, createBankAccountEvent)
     logger.info(s"Generated an event for creating a new bank account")
-
-    isEventPublishedFuture.map { isEventPublished =>
-    if(isEventPublished) {
-      logger.info(s"Event published successfully to the $stream data stream")
-      createBankAccountEvent
-    }
-    else {
-      logger.error(s"Failure occurs publishing the event to $stream data stream")
-      throw new Exception("Event not published")
-    }
-    }
+    checkAndGetPublishedEvent(publishEventResultFuture, createBankAccountEvent)
   }
 
   def creditBankAccountEvent(accountNumber: UUID, amountToCredit: Double): Future[BankAccountEvent] = ???
 
   def debitBankAccountEvent(accountNumber: UUID, amountToDebit: Double): Future[BankAccountEvent] = ???
+
+  private def checkAndGetPublishedEvent(publishEventResult: Future[Boolean], event: BankAccountEvent): Future[BankAccountEvent] =
+    publishEventResult.map { isEventPublished =>
+      if (isEventPublished) {
+        logger.info(s"Event published successfully to the $stream data stream")
+        event
+      }
+      else {
+        logger.error(s"Failure occurs publishing the event to $stream data stream")
+        throw new Exception("Event not published")
+      }
+    }
 
 }
